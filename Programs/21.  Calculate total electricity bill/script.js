588a1752af04ffe0723974a9ddb7990c7b8c7382
M-Talha44 <shktalhaweb@gmail.com>
2025-01-04 23:28:38 +0500

    08

--- Programs/21.  Calculate total electricity bill/script.js
+++ Programs/21.  Calculate total electricity bill/script.js
@@ -1,21 +1,22 @@
 var Unit=+prompt("To Check a Total Electricity Bill \n Enter Electricity unit charges ");
-var a=0
-if (Unit <= 50) {
-    a= 0.50 * Unit *20/100;
-    console.log(a);
+var a=0;
+
+ if (Unit <= 50) {
+   a= 0.50 * Unit ;
+    console.log(a + 20/100);
     
 } else if (Unit >50 && Unit <=150) {
-    a= 0.75 * Unit *20/100 ;
-    console.log(a);
+    a= 0.75 * Unit ;
+    console.log(a + 20/100 );
 } else if (Unit >150 && Unit <=250) {
-    a= 1.20 * Unit * 20/100;
-    console.log(a);
+    a= 1.20 * Unit ;
+    console.log(a + 20/100);
     
 } else if (Unit > 250) {
-    a= 1.50 * Unit * 20/100 ;
-    console.log(a);
+    a= 1.50 * Unit ;
+    console.log(a + 20/100);
     
 } else {
-    console.log("Invalid Number");
+    console.log("Invalid Number"); 
     
-}
+}  
